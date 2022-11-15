24459403b72d8540a21231c99c7736de39e1df5c
kangelopoulos <45442700+kangelopoulos@users.noreply.github.com>
2022-11-15 15:18:22 -0600

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,6 @@
 
 import MemoryChart from "../islands/MemoryChart.tsx";
 import Header from "../components/Header.tsx";
- console.log(Deno.memoryUsage());
 
 export default function Home() {
   console.log("hello from Home Export index.tsx");
