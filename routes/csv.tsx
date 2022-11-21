e460f37d93013ab567b6263eadd25939a4cd8dfa
Ethan Liu <eliu080893@gmail.com>
2022-11-21 15:14:10 -0500

    s

--- routes/csv.tsx
+++ routes/csv.tsx
@@ -1,10 +1,8 @@
 import UploadChart from "../islands/Upload.tsx";
 import Header from "../components/Header.tsx";
-import { useState } from "https://esm.sh/v98/preact@10.11.3/hooks/src/index.d.ts";
 import Chart from '../islands/CsvChart.tsx';
 
 export default function Csv () {
-    const[data, setData] = useState([])
 
     return(
         <>
