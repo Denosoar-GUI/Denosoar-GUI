152bfba020162da97ebafc9ef9a00a827b3970e8
RussoDavid <dr2378@gmail.com>
2022-11-21 14:14:20 -0500

    testing

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
 // delay is for async testing
 import { delay } from "https://deno.land/std@0.165.0/async/delay.ts";
+import  MemoryChart from '../islands/MemoryChart.tsx'
 
 
 // testing url test and addition tests to make sure this works
@@ -9,11 +10,12 @@ Deno.test("url test", () => {
     assertEquals(url.href, "https://deno.land/foo.js");
   });
 
-  Deno.test("hello world #1", () => {
-    const x = 1 + 2;
-    assertEquals(x, 3);
-  });
+  // Deno.test("hello world #1", () => {
+  //   const x = 1 + 2;
+  //   assertEquals(x, 3);
+  // });
 
   Deno.test('port test', () => {
-    
+    const input: any = document.getElementById('port')
+    assertEquals()
   })
