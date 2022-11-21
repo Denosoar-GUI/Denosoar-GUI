2bdd40c03ba04ddcd305ba52012fb7680ced9e20
RussoDavid <dr2378@gmail.com>
2022-11-21 14:02:32 -0500

    testing

---
+++ test/test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
+// delay is for async testing
+import { delay } from "https://deno.land/std@0.165.0/async/delay.ts";
+
+
+// testing url test and addition tests to make sure this works
+Deno.test("url test", () => {
+    const url = new URL("./foo.js", "https://deno.land/");
+    assertEquals(url.href, "https://deno.land/foo.js");
+  });
+
+  Deno.test("hello world #1", () => {
+    const x = 1 + 2;
+    assertEquals(x, 3);
+  });
+
+  Deno.test('port test', () => {
+    
+  })
