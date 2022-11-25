6bd1865deafeb257f610682cac4a0162d155d73f
RussoDavid <dr2378@gmail.com>
2022-11-25 10:07:32 -0500

    testing

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
 // delay is for async testing
-// import { delay } from "https://deno.land/std@0.165.0/async/delay.ts";
-// import  MemoryChart from '../islands/MemoryChart.tsx'
+ import { delay } from "https://deno.land/std@0.165.0/async/delay.ts";
+ import { Rhum } from "https://deno.land/x/rhum@v1.1.2/mod.ts";
+ import  MemoryChart from '../islands/MemoryChart.tsx'
+
 
 
 // testing url test and addition tests to make sure this works
@@ -15,7 +17,11 @@ Deno.test("url test", () => {
     assertEquals(x, 3);
   });
 
-  Deno.test('port test', () => {
-    //const input: any = document.getElementById('port')
-    assertEquals(2,2)
+
+  Rhum.testPlan('../islands/MemoryChart.tx', () => {
+    Rhum.testSuite('MemoryChart()', () => {
+      Rhum.testCase('port should start when button is clicked', () => {
+        const buttonClick = 
+      })
+    })
   })
