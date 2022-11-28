85aab949b690e64a9325d724897095ebfd86c0d7
mohammed <mohammedsebbagh@gmail.com>
2022-11-28 15:16:15 -0500

    minor changes

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,10 @@
 import { AppProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
-
 export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
         <link rel="stylesheet" href="/app.css" />
-        <link rel="icon" href="../components/super-deno.png"/>
       </Head>
       <Component />
     </>
